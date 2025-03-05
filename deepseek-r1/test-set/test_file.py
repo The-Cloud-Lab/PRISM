21c18a5c3dd9d493731f0fcb33508852ba98eadf
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2025-03-04 16:29:58 -0800

    Update test_file.py

--- deepseek-r1/test-set/test_file.py
+++ deepseek-r1/test-set/test_file.py
@@ -1,8 +1,7 @@
-import numpy as np
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+b = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
 
-a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-b = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+res = [[a[i][j] + b[i][j] for j in range(len(a[0]))] for i in range(len(a))]
 
-res = a + b
-
-print(res)
+for r in res:
+    print(r)
